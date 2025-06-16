eefcb55f36aec5bd7dfdccf35988f181addad898
Hayaman203 <tranviethuy05052003@gmail.com>
2025-06-16 15:58:14 +0700

    Create project

---
+++ public/admin/js/script.js
@@ -0,0 +1,189 @@
+// Button Status
+const buttonsStatus = document.querySelectorAll("[button-status]");
+if (buttonsStatus.length > 0) {
+  let url = new URL(window.location.href);
+
+  buttonsStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+      window.location.href = url.href;
+    });
+  });
+}
+
+// End Button Status
+
+// Form Search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+  let url = new URL(window.location.href);
+
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  });
+}
+// End Form Search
+
+// Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]");
+if (buttonsPagination.length > 0) {
+  let url = new URL(window.location.href);
+
+  buttonsPagination.forEach((button) => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+      if (page) {
+        url.searchParams.set("page", page);
+      } else {
+        url.searchParams.delete("page");
+      }
+      window.location.href = url.href;
+    });
+  });
+}
+// End Pagination
+
+// Checkbox Multi
+const checkboxMulti = document.querySelector("[checkbox-multi]");
+if (checkboxMulti) {
+  const inputCheckall = checkboxMulti.querySelector("input[name='checkall']");
+  const inputsId = checkboxMulti.querySelectorAll("input[name='id']");
+  inputCheckall.addEventListener("click", () => {
+    if (inputCheckall.checked) {
+      inputsId.forEach((input) => {
+        input.checked = true;
+      });
+    } else {
+      inputsId.forEach((input) => {
+        input.checked = false;
+      });
+    }
+  });
+
+  inputsId.forEach((input) => {
+    input.addEventListener("click", () => {
+      const countChecked = checkboxMulti.querySelectorAll(
+        "input[name='id']:checked"
+      );
+      if (countChecked.length == inputsId.length) {
+        inputCheckall.checked = true;
+      } else {
+        inputCheckall.checked = false;
+      }
+    });
+  });
+}
+// End Checkbox Multi
+
+// Form Change Multi
+const formChangeMulti = document.querySelector("[form-change-multi]");
+if (formChangeMulti) {
+  formChangeMulti.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const checkboxMulti = document.querySelector("[checkbox-multi]");
+    const inputsChecked = checkboxMulti.querySelectorAll(
+      "input[name='id']:checked"
+    );
+
+    const typeChange = e.target.elements.type.value;
+
+    if (typeChange == "delete-all") {
+      const isConfirm = confirm(
+        "Bạn chắc chắn muốn xoá những sản phẩm này không ?"
+      );
+
+      if (!isConfirm) {
+        return;
+      }
+    }
+
+    if (inputsChecked.length > 0) {
+      let ids = [];
+      const inputsId = formChangeMulti.querySelector("input[name='ids']");
+
+      inputsChecked.forEach((input) => {
+        const id = input.value;
+
+        if (typeChange == "change-position") {
+          const position = input
+            .closest("tr")
+            .querySelector("input[name='position']").value;
+
+          ids.push(`${id}-${position}`);
+        } else {
+          ids.push(id);
+        }
+      });
+
+      inputsId.value = ids.join(", ");
+      formChangeMulti.submit();
+    } else {
+      alert("Vui lòng chọn ít nhất một bản ghi!");
+    }
+  });
+}
+// End Form Change Multi
+
+// Show Alert
+const showAlert = document.querySelector("[show-alert]");
+if (showAlert) {
+  const time = parseInt(showAlert.getAttribute("data-time"));
+  const closeAlert = showAlert.querySelector("[close-alert]");
+
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, time);
+
+  closeAlert.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden");
+  });
+}
+// End Show Alert
+
+// Upload & Remove Preview Image
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+  const removeImage = document.querySelector("[remove-image]");
+
+  if (removeImage) {
+    removeImage.style.display = "none";
+  }
+
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+      uploadImagePreview.style.display = "block";
+      if (removeImage) {
+        removeImage.style.display = "block";
+      }
+    }
+  });
+
+  if (removeImage) {
+    removeImage.addEventListener("click", () => {
+      uploadImagePreview.src = "";
+      uploadImageInput.value = "";
+      uploadImagePreview.style.display = "none";
+      removeImage.style.display = "none";
+    });
+  }
+}
+// End Upload & Remove Preview Image
+
