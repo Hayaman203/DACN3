8fc902b6a7df19d5d6cec60c9fa3685c71dc99b7
Hayaman203 <tranviethuy05052003@gmail.com>
2025-07-15 14:59:52 +0700

    Hayashop Project

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const flash = require("express-flash");
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+require("dotenv").config();
+
+const database = require("./config/database");
+database.connect();
+
+const systemConfig = require("./config/system");
+
+const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(methodOverride('_method'));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
+
+// Flash
+app.use(cookieParser("luli"));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+// EndFlash
+
+//App Locals Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+app.use(express.static(`${__dirname}/public`));
+
+//Route
+routeAdmin(app);
+route(app);
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
